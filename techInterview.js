0cf53c155ab39b57c28aefc800fb2ea4b672a57d
polasudo <50864147+polasudo@users.noreply.github.com>
2023-10-17 16:26:17 +0200

    Update techInterview.js

--- techInterview.js
+++ techInterview.js
@@ -5,7 +5,7 @@
 // Otherwise it writes to console given number.
 
 const code = () => {
-    for (let i = 0; i < 100; i++) {
+    for (let i = 1; i <= 100; i++) {
         if (i % 3 == 0 && i % 5 == 0){
             console.log("Techbakers")
             
